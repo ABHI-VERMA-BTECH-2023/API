74f95d27c1de9a30e455dd98b6a9c3cb266da458
Abhi Verma <abhivermabtech@gmail.com>
2025-04-18 19:35:51 +0000

    COMMENT!

---
+++ API/API.js
@@ -0,0 +1,11 @@
+//https://randomuser.me/api/ ----API link
+
+const promise = new Promise((resolve, reject)=>{
+    setTimeout(()=>{console.log("Promise one");
+        resolve(); // resolve return data
+    },1000)     //1nano sec
+});
+
+promise.then(()=>{console.log("Sucessfuly done")})
+
+/workspaces/API/API/API.js
